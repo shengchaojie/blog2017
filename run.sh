af06a1fb025a27df439a5da4872a3567a2cc8bb1
shengchaojie <shengchaojie@163.com>
2017-08-20 22:56:56 +0800

     修改部署脚本

--- run.sh
+++ run.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+pid = ps -ef|grep blog-web |grep -v grep |awk '{print $2}'
+kill -9 $pid
+echo "killed $pid"
 cd /home/scj/workspace/blog2017
 git pull
 mvn clean package -Dmaven.test.skip=true
