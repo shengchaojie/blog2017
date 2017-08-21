9cd0acb332018f9071960adb184ea5e9f79f0eb0
shengchaojie <shengchaojie@163.com>
2017-08-21 09:46:38 +0800

    shell update

--- run.sh
+++ run.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-pid= ps -ef|grep blog-web |grep -v grep |awk '{print $2}'
+pid=ps -ef|grep blog-web |grep -v grep |awk '{print $2}'
+if (($pid >0))
+then
+echo 'pid is $pid'
 kill  $pid
 echo "killed $pid"
+else
+echo 'no blog.jar running'
+fi
 cd /home/scj/workspace/blog2017
 git pull
 mvn clean package -Dmaven.test.skip=true
