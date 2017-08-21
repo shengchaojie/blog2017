58af14de2fb2da01da554e91ee9cbd929ced9973
shengchaojie <shengchaojie@163.com>
2017-08-21 10:57:39 +0800

    update

--- run.sh
+++ run.sh
@@ -10,6 +10,9 @@ echo 'no blog.jar running'
 fi
 cd /home/scj/workspace/blog2017
 git pull
-mvn clean package -Dmaven.test.skip=true
+echo 'maven build start'
+mvn clean package -Dmaven.test.skip=true>/dev/null
+echo 'maven build end'
 cd blog-web/target
 java -jar blog.jar --spring.profiles.active=prd&
+echo 'project running'
